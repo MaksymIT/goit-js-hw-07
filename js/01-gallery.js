b47bba8f82aa7bc1483641c7fe3dd2ba87c5d027
MaksymIT <maksymkibisg@gmail.com>
2023-11-02 22:16:01 +0100

    Commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items";
 
-console.log(galleryItems);
+galleryList.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  // const dataPreview = event.target.dataset.source;
+  // const imgDesc = event.target.alt;
+
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" alt="${event.target.alt}"/>
+  `,{
+    onShow: () => { document.addEventListener("keydown", onEscPress) },
+    onClose: () => {document.removeEventListener("keydown", onEscPress)}
+  });
+  instance.show();
+
+  function onEscPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  };
+}
+
